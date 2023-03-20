8354bfae7afa38af06dc2bd64c06bcdaf51e55d6
Jerry-MC137 <jeremychamberlin5@gmail.com>
2023-03-20 08:53:02 +0300

    initial commit for a standard tcp client to send and recieve some data

--- tcpclient.py
+++ tcpclient.py
@@ -1,18 +1,18 @@
 import socket
 
-target_host = "www.google.com"
-target_port = 80
+TARGET_HOST = "www.google.com"
+TARGET_PORT = 80
 
 #create socket object
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 #connect the client
-client.connect((target_host, target_port))
+client.connect((TARGET_HOST, TARGET_PORT))
 
-# send data
+#send data
 client.send(b"GET / HTTP/1.1\r\nHost: google.com\r\n\r\n")
 
-# receive data
+#receive some data
 response = client.recv(4096)
 
 print(response.decode())
